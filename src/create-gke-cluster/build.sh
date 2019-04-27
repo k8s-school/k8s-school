7baf58ed44721bfc3008846e801010a48615317e
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2019-04-27 20:19:27 +0100

    Add go proto

--- src/create-gke-cluster/build.sh
+++ src/create-gke-cluster/build.sh
@@ -1,1 +1,1 @@
-docker run --rm -v "$PWD":/usr/src/myapp -w /usr/src/myapp golang:1.12.4 go build -v
+docker run --rm -v "$PWD":/usr/src/create-gke-cluster -w /usr/src/create-gke-cluster golang:1.12.4 go build -v
--- src/create-gke-cluster/main.go
+++ src/create-gke-cluster/main.go
@@ -21,17 +21,29 @@ func Shellout(command string) (error, string, string) {
 
 func main() {
 
-    command = 'gcloud beta container --project "$PROJECT" clusters create "$CLUSTER" --zone "$REGION" \
+    project := "coastal-sunspot-206412"
+    cluster := "cluster-1"
+    region := "us-central1"
+    zone := "us-central1-a"
+    num_nodes := 2
+    network := fmt.Sprintf("projects/%v/global/networks/default", project)
+    subnetwork := fmt.Sprintf("projects/%v/regions/%v/subnetworks/default", project, region)
+    min_nodes := 2
+    max_nodes := 4
+
+    cmd_tpl := `gcloud beta container --project "%v" clusters create "%v" --zone "%v" \
     --no-enable-basic-auth --cluster-version "1.11.8-gke.6" --machine-type "n1-standard-2" \
     --image-type "COS" --disk-type "pd-standard" --disk-size "100" \
     --scopes "https://www.googleapis.com/auth/devstorage.read_only","https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/monitoring","https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/service.management.readonly","https://www.googleapis.com/auth/trace.append" \
-    --preemptible --num-nodes "2" --no-enable-cloud-logging --no-enable-cloud-monitoring \
-    --no-enable-ip-alias --network "projects/coastal-sunspot-206412/global/networks/default" \
-    --subnetwork "projects/coastal-sunspot-206412/regions/us-central1/subnetworks/default" \
-    --enable-autoscaling --min-nodes "2" --max-nodes "4" \
-    --addons HorizontalPodAutoscaling,HttpLoadBalancing --enable-autoupgrade --enable-autorepair'
+    --preemptible --num-nodes "%v" --no-enable-cloud-logging --no-enable-cloud-monitoring \
+    --no-enable-ip-alias --network "%v" --subnetwork "%v" \
+    --enable-autoscaling --min-nodes "%v" --max-nodes "%v" \
+    --addons HorizontalPodAutoscaling,HttpLoadBalancing --enable-autoupgrade --enable-autorepair`
 
-    err, out, errout := Shellout("ls -ltr")
+    cmd := fmt.Sprintf(cmd_tpl, project, cluster, zone, num_nodes, network, subnetwork, min_nodes, max_nodes)
+
+    fmt.Println(cmd)
+    err, out, errout := Shellout(cmd)
     if err != nil {
         log.Printf("error: %v\n", err)
     }
