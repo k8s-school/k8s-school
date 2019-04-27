037a9f4975e6c8b3bdb411aa25c5d002d8648fbf
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2019-04-27 19:09:12 +0100

    up

--- homefs/advanced/elasticsearch/generate-log.sh
+++ homefs/advanced/elasticsearch/generate-log.sh
@@ -1,7 +1,13 @@
+#!/bin/sh
+
 set -e
 
 # TODO delete nginw with label
+kubectl delete pod,service -l "app=nginx" -n logging
 kubectl run nginx --generator=run-pod/v1 --image=nginx -n logging
+kubectl label pod -n logging nginx "app=nginx"
+kubectl create service clusterip -n logging nginx --tcp 80
+kubectl label service -n logging nginx "app=nginx"
 
 # Wait for logging:nginx to be in running state
 while true
@@ -13,5 +19,4 @@ do
     fi
 done
 
-kubectl port-forward -n logging nginx 8081:80&
 while true; do curl localhost:8081; sleep 2; done
--- homefs/advanced/elasticsearch/install.sh
+++ homefs/advanced/elasticsearch/install.sh
@@ -1,16 +1,32 @@
+#!/bin/sh
+
 set -x
 set -e
 
 # WARN does not work in dind
-# see https://platform9.com/blog/kubernetes-logging-and-monitoring-the-elasticsearch-fluentd-and-kibana-efk-stack-part-2-elasticsearch-configuration/
+# see https://platform9.com/blog/kubernetes-"$NS"-and-monitoring-the-elasticsearch-fluentd-and-kibana-efk-stack-part-2-elasticsearch-configuration/
 
-kubectl create namespace logging
+NS="logging"
 
-helm install stable/elasticsearch --namespace logging --name elasticsearch --set data.terminationGracePeriodSeconds=0 --set master.persistence.enabled=false     --set data.persistence.enabled=false
+kubectl delete ns -l name="logging"
+kubectl create namespace "$NS"
+kubectl label ns "$NS" name="logging"
+
+# Install elasticsearch
+helm install stable/elasticsearch --namespace "$NS" --name elasticsearch --set data.terminationGracePeriodSeconds=0 
+# \
+#    --set master.persistence.enabled=false --set data.persistence.enabled=false
+
+# Install fluentd
 helm repo add kiwigrid https://kiwigrid.github.io
-helm install --name fluentd --namespace logging kiwigrid/fluentd-elasticsearch --set elasticsearch.host=elasticsearch-client.logging.svc.cluster.local,elasticsearch.port=9200
-helm install --name kibana --namespace logging stable/kibana --set env.ELASTICSEARCH_URL=http://elasticsearch-client.logging.svc.cluster.local:9200
-generate-log.sh &
+helm install --name fluentd --namespace "$NS" kiwigrid/fluentd-elasticsearch --set elasticsearch.host=elasticsearch-client."$NS".svc.cluster.local,elasticsearch.port=9200
 
-export POD_NAME=$(kubectl get pods --namespace logging -l "app=kibana,release=kibana" -o jsonpath="{.items[0].metadata.name}")
-kubectl port-forward -n logging "$POD_NAME" 5601&
+# Install Kibana
+helm install --name kibana --namespace "$NS" stable/kibana --set env.ELASTICSEARCH_URL=http://elasticsearch-client."$NS".svc.cluster.local:9200
+
+# Generate logs
+./generate-log.sh > /dev/null &
+
+POD_NAME=$(kubectl get pods --namespace "$NS" -l "app=kibana,release=kibana" -o jsonpath="{.items[0].metadata.name}")
+kubectl port-forward -n "$NS" "$POD_NAME" 5601&
+# In Kibana, go to "Discover", add "logstash*" index and "@timestamp" filter, then go to "Discover"
