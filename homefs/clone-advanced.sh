9639416eb3369915195e0a501a4b2621d0c97e56
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2019-05-11 14:29:37 +0200

    upd

--- homefs/clone-advanced.sh
+++ homefs/clone-advanced.sh
@@ -3,4 +3,5 @@
 set -e
 
 git clone https://github.com/luksa/kubernetes-in-action.git
-git clone https://gitlab.com/fjammes/k8s-advanced.git
+git clone https://github.com/fjammes/k8s-exercices
+#git clone https://gitlab.com/fjammes/k8s-advanced
