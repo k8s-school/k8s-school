46b8e22c0bd27713df2e9bf277cf8b92a05e820a
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2019-06-27 10:15:12 +0200

    Reduce env.sh

--- homefs/gcp/env.sh
+++ homefs/gcp/env.sh
@@ -5,15 +5,6 @@
 
 # fjammes15
 PROJECT="coastal-sunspot-206412" 
-IMAGE="ubuntu-1804-bionic-v20190404"
-IMAGE_PROJECT="ubuntu-os-cloud"
-INSTANCE_PREFIX="node-"
-MACHINE_TYPE="n1-standard-2"
-
-US_CLUSTERS="cluster-1 cluster-2 cluster-3"
-EU_CLUSTERS="cluster-4 cluster-5 cluster-6"
-US_REGION="us-central1-a"
-EU_REGION="europe-north1-a"
 
 CLUSTER="cluster-1"
 REGION="us-central1-a"
