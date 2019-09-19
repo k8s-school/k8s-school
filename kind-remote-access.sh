1dcbbbd12a0f377c8528e68ce26acbf77bee02b0
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2019-09-19 17:09:46 +0200

    upd

--- kind-remote-access.sh
+++ kind-remote-access.sh
@@ -10,4 +10,6 @@ DIR=$(cd "$(dirname "$0")"; pwd -P)
 KIND_SERVER="clrqserv01.in2p3.fr"
 
 scp -r "$KIND_SERVER":~/.kube/kind-config-kind ~/src/k8s-school/homefs/.kube/config
+# Optional: retrieve certs
+# scp -r "$KIND_SERVER":~/src/k8s-school/homefs/.certs ~/src/k8s-school/homefs/
 ssh -nNT -L 35777:localhost:35777 clrqserv01.in2p3.fr
