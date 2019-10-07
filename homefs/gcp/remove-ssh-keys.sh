97c2aadc7deb7682184b95d744d0a404db4b6ae5
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2019-10-07 22:21:46 +0200

    upd

--- homefs/gcp/remove-ssh-keys.sh
+++ homefs/gcp/remove-ssh-keys.sh
@@ -5,4 +5,8 @@
 
 set -e
 
+echo "Remove ssh keys:"
 gcloud compute os-login ssh-keys list | grep -v FINGERPRINT | xargs -I {} -n 1 gcloud compute os-login ssh-keys remove --key={}
+
+echo "List ssh keys:"
+gcloud compute os-login ssh-keys list 
